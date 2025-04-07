ec89425e591240c4d60298b71158543770641e30
neto72a1 <121133556+neto72a1@users.noreply.github.com>
2025-04-07 14:45:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class BoasVindas
+{
+
+    public static void Main(string[] args)
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        string mensagem = $"Olá, {nome}! Seja bem-vindo(a)!";
+
+        Console.WriteLine(mensagem);
+
+        Console.ReadKey();
+    }
+}
